1cee2535c839cadbe716a410c8fc6f87ba13c433
Daniel Curcio <crcdnl02e28m208a@studenti.unical.it>
2024-02-27 18:48:45 +0100

    Inizio struttura del Tab3 "Linee"

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+interface Company {
+  name: string;
+  lines: string[];
+}
+
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
@@ -7,6 +12,28 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
+  companies: Company[] = [
+    {
+      name: 'Azienda A',
+      lines: ['121', '122', '138']
+    },
+    {
+      name: 'Azienda B',
+      lines: ['10', '11']
+    }
+  ];
+
+  selectedCompany: Company | null = null;
+
   constructor() {}
+
+  toggleCompany(company: Company) {
+    this.selectedCompany = (this.selectedCompany === company) ? null : company;
+  }
+
+  search(event: CustomEvent) {
+    const query = event.detail.value;
+    console.log(query);
+  }
 
 }
