5f6127a0a8bb8e7178e6ddec49b18e5148405857
Daniel Curcio <crcdnl02e28m208a@studenti.unical.it>
2024-03-23 10:58:52 +0100

    Added initializeTodayHistory Modified updateStopReached by replacing placeholders in the array

--- backend/src/main/java/com/example/busbus_backend/controller/api/RouteService.java
+++ backend/src/main/java/com/example/busbus_backend/controller/api/RouteService.java
@@ -15,10 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.ExecutionException;
 
 @RestController
@@ -109,7 +106,14 @@ public class RouteService {
                             // Se è domenica, aggiungere l'orario all'oggetto "sunday" invece di "week"
                             List<String> stopTimes = isSunday() ? timetable.getSunday().get(stopIndex) : timetable.getWeek().get(stopIndex);
                             if (stopTimes != null) {
-                                stopTimes.add(currentTime);
+                                for(int i = 0; i < stopTimes.size(); i++){
+                                    System.out.println("stopTimes.get(i): " + stopTimes.get(i));
+                                    if(stopTimes.get(i) != null && stopTimes.get(i).equals("-")){
+                                        System.out.println("stopTimes.get(i) == '-'");
+                                        stopTimes.set(i, currentTime);
+                                        break;
+                                    }
+                                }
                             } else {
                                 stopTimes = new ArrayList<>();
                                 stopTimes.add(currentTime);
@@ -130,11 +134,132 @@ public class RouteService {
                             return new ResponseEntity<>(true, HttpStatus.OK);
                         }
                     }
+                    else{
+                        // Inizializza l'oggetto "history" per la data odierna
+                        ResponseEntity<Boolean> initializationResult = initializeTodayHistory(routeId);
+
+                        // Controlla se l'inizializzazione ha avuto successo
+                        if (Boolean.TRUE.equals(initializationResult.getBody())) {
+                            // L'inizializzazione è avvenuta con successo, ora esegui l'aggiornamento dello stopReached
+                            ResponseEntity<Boolean> updateResult = updateStopReached(routeId, stopIndex, direction);
+
+                            // Controlla se l'aggiornamento ha avuto successo
+                            if (Boolean.TRUE.equals(updateResult.getBody())) {
+                                // L'aggiornamento è avvenuto con successo
+                                return new ResponseEntity<>(true, HttpStatus.OK);
+                            } else {
+                                // Gestisci eventuali errori nell'aggiornamento dello stopReached
+                                return new ResponseEntity<>(false, HttpStatus.INTERNAL_SERVER_ERROR);
+                            }
+                        } else {
+                            // Gestisci eventuali errori nell'inizializzazione della history
+                            return new ResponseEntity<>(false, HttpStatus.INTERNAL_SERVER_ERROR);
+                        }
+                    }
                 }
             }
             return new ResponseEntity<>(false, HttpStatus.NOT_FOUND);
         } catch (InterruptedException | ExecutionException e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    private ResponseEntity<Boolean> initializeTodayHistory(String routeId) {
+        Firestore db = FirestoreClient.getFirestore();
+        CollectionReference routes = db.collection(COLLECTION_NAME);
+
+        try {
+            DocumentReference routeRef = routes.document(routeId);
+            DocumentSnapshot routeSnapshot = routeRef.get().get();
+            if (routeSnapshot.exists()) {
+                // Ottenere il campo "history" dal documento del percorso
+                Map<String, Route.Data> history = routeSnapshot.toObject(Route.class).getHistory();
+
+                // Verificare se l'oggetto history esiste
+                if (history != null) {
+                    // Creare un nuovo oggetto "history" con un oggetto "Data" vuoto per la data odierna
+                    Map<String, Route.Data> newHistory = new HashMap<>();
+                    newHistory.put(getCurrentDate(), new Route.Data());
+                    System.out.println("newHistory: " + newHistory);
+
+                    // prendi il campo timetable dal documento del percorso
+                    Schedule timetable = routeSnapshot.toObject(Route.class).getTimetable();
+                    System.out.println("timetable: " + timetable);
+
+                    // per ogni mappa di timetable, crea una mappa con il segnaposto "-" per ogni valore non null dell'array
+                    // crea solo week o solo sunday a seconda del giorno della settimana
+
+                    // Utilizzo del metodo per sostituire i valori non nulli con il segnaposto per entrambe le direzioni (forward e back)
+                    Map<String, List<String>> forwardDay = isSunday() ? timetable.getForward().getSunday() : timetable.getForward().getWeek();
+                    replaceNonNullValuesWithPlaceholder(forwardDay);
+                    System.out.println("Forward day: " + forwardDay);
+
+                    Map<String, List<String>> backDay = isSunday() ? timetable.getBack().getSunday() : timetable.getBack().getWeek();
+                    replaceNonNullValuesWithPlaceholder(backDay);
+                    System.out.println("Back day: " + backDay);
+
+                    // Aggiungi a newHistory l'oggetto Data con le mappe appena create
+                    newHistory.get(getCurrentDate()).setForward(new Schedule.Timetable());
+                    newHistory.get(getCurrentDate()).setBack(new Schedule.Timetable());
+                    newHistory.get(getCurrentDate()).getForward().setWeek(timetable.getForward().getWeek());
+                    newHistory.get(getCurrentDate()).getForward().setSunday(timetable.getForward().getSunday());
+                    newHistory.get(getCurrentDate()).getBack().setWeek(timetable.getBack().getWeek());
+                    newHistory.get(getCurrentDate()).getBack().setSunday(timetable.getBack().getSunday());
+                    System.out.println("newHistory: " + newHistory);
+
+
+
+
+
+                    // Aggiornare il campo "history" nel documento del percorso
+                    // Se ci sono piu di 7 giorni di history, rimuovere la data piu vecchia
+                    db.runTransaction(transaction -> {
+                        DocumentSnapshot routeSnapshotAgain = transaction.get(routeRef).get();
+                        Route route = routeSnapshotAgain.toObject(Route.class);
+                        if (route != null) {
+                            route.getHistory().putAll(newHistory);
+
+                            if(route.getHistory().size() > 7){
+                                // Ordina le chiavi delle date in ordine ascendente
+                                List<String> dates = new ArrayList<>(route.getHistory().keySet());
+                                Collections.sort(dates, new Comparator<String>() {
+                                    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+
+                                    @Override
+                                    public int compare(String date1, String date2) {
+                                        LocalDate localDate1 = LocalDate.parse(date1, formatter);
+                                        LocalDate localDate2 = LocalDate.parse(date2, formatter);
+                                        return localDate1.compareTo(localDate2);
+                                    }
+                                });
+                                route.getHistory().remove(dates.get(0));
+                            }
+
+                            transaction.update(routeRef, "history", route.getHistory());
+                        }
+                        return true;
+                    });
+
+
+                    return new ResponseEntity<>(true, HttpStatus.OK);
+                }
+            }
+            return new ResponseEntity<>(false, HttpStatus.NOT_FOUND);
+        } catch (InterruptedException | ExecutionException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void replaceNonNullValuesWithPlaceholder(Map<String, List<String>> day) {
+        for (Map.Entry<String, List<String>> entry : day.entrySet()) {
+            List<String> times = new ArrayList<>();
+            for (String time : entry.getValue()) {
+                if (time != null)
+                    times.add("-");
+                else
+                    times.add(null);
+            }
+            day.put(entry.getKey(), times);
         }
     }
 
