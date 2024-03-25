1bd5eb427e24eb7fb932f5f4a2158246df51d717
Daniel Curcio <crcdnl02e28m208a@studenti.unical.it>
2024-03-25 20:34:28 +0100

    Fixed FirebaseInitialization (impossible to do mvn package)

--- backend/src/main/java/com/example/busbus_backend/persistence/FirestoreInitializer.java
+++ backend/src/main/java/com/example/busbus_backend/persistence/FirestoreInitializer.java
@@ -3,10 +3,11 @@ package com.example.busbus_backend.persistence;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 import javax.annotation.PostConstruct;
-import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 @Component
 public class FirestoreInitializer {
@@ -14,13 +15,16 @@ public class FirestoreInitializer {
     @PostConstruct
     public void initialize() {
         try {
-            FileInputStream serviceAccount =
-                    new FileInputStream("backend/src/main/resources/serviceAccountKey.json");
+            // Carica il file serviceAccountKey.json dalla classpath del progetto
+            InputStream serviceAccount =
+                    new ClassPathResource("serviceAccountKey.json").getInputStream();
 
+            // Configura le opzioni di Firebase utilizzando le credenziali caricate
             FirebaseOptions options = new FirebaseOptions.Builder()
                     .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                     .build();
 
+            // Inizializza FirebaseApp con le opzioni configurate
             FirebaseApp.initializeApp(options);
         } catch (IOException e) {
             e.printStackTrace();
