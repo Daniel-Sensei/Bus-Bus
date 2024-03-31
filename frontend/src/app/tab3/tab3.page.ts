ab97397328d57eb47d185c3633dabb82c068a28a
Daniel Curcio <crcdnl02e28m208a@studenti.unical.it>
2024-03-31 20:36:54 +0200

    Added nested accordation to show the (full) timetable of a route

--- frontend/src/app/tab3/tab3.page.ts
+++ frontend/src/app/tab3/tab3.page.ts
@@ -17,6 +17,7 @@ export class Tab3Page {
   routes: Map<string,any[]> = new Map();
   filteredRoutes: Map<string,any[]> = new Map(); // Aggiungi un array per i dati filtrati
   loading: boolean = true; // Aggiungi una variabile per il caricamento
+  selectedSegment: string = 'default'; //stops and buses
 
   constructor(private routeService: RouteService) {}
 
@@ -54,7 +55,30 @@ export class Tab3Page {
       }
     }
   }
+
+
+
+getStopTimetable(company: string, route: any, direction: string, stopIndex: number) {
+  //find route in this.routes
+  for(let [companyName, lines] of this.routes) {
+    if(companyName === company) {
+      for(let line of lines) {
+        if(line.code === route.code) {
+          return line.timetable[direction][stopIndex].join(" - ");
+        }
+      }
+    }
+  }
+
+
+}
   
+  clearSearch() {
+    this.filteredRoutes = this.routes;
+  }
+  segmentChanged(event: any) {
+    this.selectedSegment = event.detail.value;
+  }
   
 
 }
